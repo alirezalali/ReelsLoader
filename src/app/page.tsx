ccffb9a23794936389651553b73e6a79982f4083
Unknown <122030730+alirezalali@users.noreply.github.com>
2025-01-12 17:21:53 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col py-8">
       <h1 className="text-balance mb-2 text-center text-4xl font-extrabold">
-      Native IG
+      ReelsLoader
       </h1>
       <p className="text-balance mb-4 text-center text-xl font-extrabold">Instagram Video Downloader</p>
       <section className="flex flex-col items-center justify-center gap-4">
